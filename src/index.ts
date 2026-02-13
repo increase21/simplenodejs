9710d177df422f6a84db2c2d8fc1dc10ff0e0dff
Increase <engr.increase@gmail.com>
2026-02-13 01:10:01 +0100

    Exported SimpleJsPlugin

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { CreateSimpleJsHttpServer } from "./server";
 export { SetRequestCORS, SetRateLimiter, SetBodyParser } from "./utils/helpers"
+export * from "./utils/simplePlugins"
 export type { SimpleJsPrivateMethodProps } from "./typings/general";
