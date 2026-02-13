f4a219edf1fbf69d36c7461ae2b4a672b49fb7d3
Increase <engr.increase@gmail.com>
2026-02-13 01:34:04 +0100

    Fixed internal file missing

--- src/utils/simpleController.ts
+++ src/utils/simpleController.ts
@@ -1,10 +1,5 @@
-import { HttpMethod, ObjectPayload, RequestObject, ResponseObject } from "../typings/general";
-type SubRequestHandler = Partial<Record<HttpMethod, (params: PrivateMethodProps) => any>>;
-
-interface PrivateMethodProps {
-  id?: string;
-  idMethod?: Partial<Record<HttpMethod, "required" | "optional">>;
-}
+import { HttpMethod, ObjectPayload, RequestObject, ResponseObject, SimpleJsPrivateMethodProps } from "../typings/general";
+type SubRequestHandler = Partial<Record<HttpMethod, (params: SimpleJsPrivateMethodProps) => any>>;
 
 export class SimpleNodeJsController {
   protected req!: RequestObject;
@@ -26,18 +21,14 @@ export class SimpleNodeJsController {
 
   __checkContext() { }
 
-  protected RunRequest(handlers: SubRequestHandler, params?: PrivateMethodProps) {
+  protected RunRequest(handlers: SubRequestHandler, params: SimpleJsPrivateMethodProps) {
     const method = this.req.method?.toLowerCase() as HttpMethod | undefined;
 
-    if (!method) {
-      return this.res.status(400).json({ code: 400, error: "Invalid HTTP Method" });
-    }
+    if (!method) return this.res.status(400).json({ code: 400, error: "Invalid HTTP Method" });
 
     const runFn = handlers[method];
 
-    if (typeof runFn !== "function") {
-      return this.res.status(405).json({ code: 405, error: "Method Not Allowed" });
-    }
+    if (typeof runFn !== "function") return this.res.status(405).json({ code: 405, error: "Method Not Allowed" });
 
     // ID validation rules
     if (params && params.id && (!params.idMethod || !params.idMethod[method])) {
@@ -48,10 +39,6 @@ export class SimpleNodeJsController {
       return this.res.status(404).json({ code: 404, error: "Resource not found" });
     }
 
-    return runFn({
-      ...(params || {}),
-      req: this.req,
-      res: this.res,
-    } as PrivateMethodProps);
+    return runFn(params as SimpleJsPrivateMethodProps);
   }
 }
